fe414681b3f5aff93bd2465b9b31ad1a4697326d
Kenny Tsao <tsao.kenny@gmail.com>
2023-06-27 19:35:12 -0400

    lint

--- src/main.py
+++ src/main.py
@@ -2,17 +2,17 @@ import customtkinter
 
 
 class App(customtkinter.CTk):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
         self.geometry("400x150")
 
         self.button = customtkinter.CTkButton(self, text="A Button", command=self.button_callback)
         self.button.pack(padx=20, pady=20)
 
-    def button_callback(self):
+    def button_callback(self) -> None:
         print("Hello")
-   
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app = App()
     app.mainloop()
